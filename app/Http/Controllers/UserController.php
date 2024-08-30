a585627026b767189cbead92f816855230dc093b
lukastyas <tyasadewa@gmail.com>
2024-08-30 10:13:30 +0700

    auth

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class UserController extends Controller
+{
+    //index
+    public function index()
+    {
+        $users = User::where('name', 'like', '%' . request('name') . '%')
+            ->orderBy('id', 'desc')
+            ->paginate(10);
+        return view('pages.users.index', compact('users'));
+    }
+
+    public function create()
+    {
+        return view('pages.users.create');
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required',
+            'email' => 'required|email|unique:users,email',
+            'password' => 'required|min:8',
+        ]);
+
+        User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'phone' => $request->phone,
+            'role' => $request->role,
+            'password' => Hash::make($request->password),
+            'position' => $request->position,
+            'department' => $request->department,
+        ]);
+
+        return redirect()->route('users.index')->with('success', 'User Created Successfully');
+    }
+    public function edit(User $user)
+    {
+        return view('pages.users.edit', compact('user'));
+    }
+    public function update(Request $request, User $user)
+    {
+        $request->validate([
+            'name' => 'required',
+            'email' => 'required|email',
+        ]);
+
+        $user->update([
+            'name' => $request->name,
+            'email' => $request->email,
+            'phone' => $request->phone,
+            'role' => $request->role,
+            'position' => $request->position,
+            'department' => $request->department,
+        ]);
+
+        //if password failed
+        if ($request->password) {
+            # code...
+            $user->update([
+                'password' => Hash::make($request->password),
+            ]);
+        }
+        return redirect()->route('users.index')->with('success', 'User Updated Successfully');
+    }
+
+    public function destroy(User $user)
+    {
+        $user->delete();
+        return redirect()->route('users.index')->with('success', 'User Deleted Successfully');
+    }
+}
